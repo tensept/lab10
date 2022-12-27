69d1d5a750b7a8166108bcd22cced2abf306e323
tensept <vankaew58@gmail.com>
2022-12-27 16:18:09 +0700

    lab10_2 compeleted

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 int main(){	
+	int year = 0;
+	double loan,interest_rate,interest,payment,total,newbalance;
 	cout << "Enter initial loan: ";
 	cout << "Enter interest rate per year (%): ";
 	cout << "Enter amount you can pay per year: ";
@@ -20,13 +22,19 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
+	do{
+		year = year + 1;
+		interest = loan*(interest_rate/100);
+		total = loan + interest;
+		newbalance = total - payment;
+	}while(newbalance > 0)
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
+	cout << setw(13) << left << year; 
+	cout << setw(13) << left << loan;
+	cout << setw(13) << left << interest;
+	cout << setw(13) << left << total;
+	cout << setw(13) << left << payment;
+	cout << setw(13) << left << newbalance;
 	cout << "\n";	
 	
 	return 0;
